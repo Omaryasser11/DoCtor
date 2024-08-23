b9d18b73050f9340fe6c63afd6744f9adb32f55e
Sara Adel <102744772+saraa-adel@users.noreply.github.com>
2024-08-23 19:03:58 +0300

    Update Blog.jsx

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -2,13 +2,18 @@ import React, { useState,useEffect} from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import Pagination from '@mui/material/Pagination';
-import style from './Blog.scss';
+import './Blog.scss';
 import cardImg from '../../assets/images/Ultrasound_Blog3-1024x576.jpg';
 import { isFlippedState } from '../../store/index.js';
 import { useRecoilState } from 'recoil';
+import baseUrl from '../../BaseUrl'
+
 export default function Blog() {
-  
+  const [data, setData] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
   const [currentPage, setCurrentPage] = useState(1);
+
   const cardsPerPage = 24;
   const cardData = [
     { id: 1 },
@@ -45,18 +50,33 @@ export default function Blog() {
     setCurrentPage(page);
   };
 
+  useEffect(()=>{
+    baseUrl.get('blogs')
+    .then(response=>{
+      setData(response.data.data)
+      setLoading(false)
+    })
+    .catch(error =>{
+    setError(error)
+    setLoading(false)
+    })
+  },[])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
   return (
     <>
       <Helmet>
         <title>Our Blog</title>
       </Helmet>
-      <div className="montserrat bg-light pb-5 ttte">
+      <div className="montserrat bg-light pb-5">
         {/* Header */}
-        <div className="text-white position-relative blueC header-height">
-          <div className="layerC position-absolute top-0 bottom-0 start-0 end-0 z-1"></div>
+        <div className="row gx-0 position-relative blueC mb-2" style={{ height: '318px' }}>
+          {/* <div className="layerC position-absolute top-0 bottom-0 start-0 end-0 z-1"></div> */}
           <div className='container d-flex align-items-center justify-content-center h-100'>
             <div className='mt-3 d-flex align-items-end position-absolute z-3 text-center justify-content-center' style={{ height: '200px' }}>
-              <h4 className='fs-1 fw-semibold'>Latest in Cosmetic Surgery</h4>
+              <h4 className='fs-1 fw-semibold text-white'>Latest in Cosmetic Surgery</h4>
             </div>
           </div>
         </div>
@@ -64,41 +84,44 @@ export default function Blog() {
         {/* Body of cards */}
         <div className="row gx-0">
           <div className='offset-1 col-10 p-4 row g-5'>
-            <div className="col-lg-4 col-sm-6">
-              <div className={`cursor-pointer position-relative ${style.shadow} video overflow-hidden rounded-2 bg-white`}>
+          {data.map((item) => <>
+            <div key={item.id} className="col-lg-4 col-sm-6">
+              <div className={`cursor-pointer position-relative shadow video overflow-hidden rounded-2 bg-white`}>
                 <div className='overflow-hidden'>
-                  <img src={cardImg} className='w-100 scale' alt="youtube video" />
+                  <img src={item.imageUrl} className='w-100 scale' alt="Blog image" />
                 </div>
                 <div className='m-4'>
                   <div className='my-2 position-relative d-flex flex-wrap'>
-                    <Link to={'/our-blog'} className={`small text-decoration-none darkCyan-text fw-medium ${style.linkBlog} mb-2 me-3`}>Plastic Surgery</Link>
-                    <Link to={'/our-blog'} className={`small text-decoration-none darkCyan-text fw-medium ${style.linkBlog} mb-2 me-3`}>Cosmetic Surgery</Link>
-                    <Link to={'/our-blog'} className={`small text-decoration-none darkCyan-text fw-medium ${style.linkBlog} mb-2 me-3`}>Liposuction</Link>
-                    <Link to={'/our-blog'} className={`small text-decoration-none darkCyan-text fw-medium ${style.linkBlog} mb-2 me-3`}>post-op recovery</Link>
+                  {item.categories.map((category) => <>
+                    <Link key={category.id} to={'/Blog'} className={`small text-decoration-none darkCyan-text fw-medium linkBlog mb-2 me-3`}>{category.name}</Link>
+                  </>)}
                   </div>
                   <div>
-                    <h4 className='fs-5 fw-semibold text-black'>Revolutionizing Rhinoplasty</h4>
-                    <p className='text-secondary-emphasis my-3'>Rhinoplasty, commonly known as a "nose job," has been a popular cosmetic procedure for decades.…</p>
-                    <Link to={'/our-blog'} className={`small text-decoration-none fw-medium text-secondary-emphasis d-flex mb-2 me-3 ${style.circleLink}`}>
-                      <div className={`rounded-circle ${style.sizeCircle} ${style.shadowCircle} me-3`}></div>
-                      Dr. William Miami<br />
-                      May 18, 2024
+                    <h4 className='fs-5 fw-semibold text-black'>{item.title}</h4>
+                    <p className='text-secondary-emphasis my-3'>{item.description}</p>
+                    <Link to={'/Blog'} className={`small text-decoration-none fw-medium text-secondary-emphasis d-flex mb-2 me-3 circleLink`}>
+                      <div className={`rounded-circle sizeCircle shadowCircle me-3`}></div>
+                      {item.author.name}<br />
+                      {item.writtenOn.split('T')[0]}
                     </Link>
                   </div>
                 </div>
               </div>
             </div>
+            </>)}
           </div>
         </div>
 
         {/* Pagination */}
         <nav className='mt-4 mb-5'>
+          <div className='d-flex justify-content-center align-items-center w-100 small'>
           <Pagination
             count={count}
             page={currentPage}
             onChange={handlePageChange}
             color="primary"
           />
+          </div>
         </nav>
       </div>
     </>
